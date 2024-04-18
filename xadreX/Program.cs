251be18dd3680de9ec74ceb452590f3d7f21f3b5
augustocsmiranda <augustocsmiranda@gmail.com>
2024-04-18 14:19:15 -0300

    Começando a Implementar o Tabuleiro

--- xadreX/Program.cs
+++ xadreX/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadreX
 {
@@ -7,13 +7,10 @@ namespace xadreX
     {
         static void Main(string[] args)
         {
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            P = new Posicao(3, 4);
 
-            Console.WriteLine("Posição: " + P);
-
-            Console.WriteLine();
+           
         } 
     }
 }
