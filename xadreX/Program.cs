bfba182e608075cd6b9cf6dd66731b55e764a4a4
augustocsmiranda <augustocsmiranda@gmail.com>
2024-04-25 15:00:20 -0300

    testes iniciais com movimentos possiveis das peças

--- xadreX/Program.cs
+++ xadreX/Program.cs
@@ -19,6 +19,14 @@ namespace xadreX
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posisoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posisoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
