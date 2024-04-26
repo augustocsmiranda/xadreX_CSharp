a2e9a20fb710ca41d29621cafb84f6f2be60498a
augustocsmiranda <augustocsmiranda@gmail.com>
2024-04-26 13:45:08 -0300

    Controlando as peças capturadas

--- xadreX/Program.cs
+++ xadreX/Program.cs
@@ -16,11 +16,8 @@ namespace xadreX
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando Jogada: " + partida.jogadorAtual);
-
+                        Tela.imprimirPartida(partida);
+                        
                         Console.WriteLine();
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
