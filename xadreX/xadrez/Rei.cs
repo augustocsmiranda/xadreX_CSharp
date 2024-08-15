665533104396766a89ea5909d2a56f007f92e87b
augustocsmiranda <augustocsmiranda@gmail.com>
2024-08-15 13:42:44 -0300

    Roque Grande

--- xadreX/xadrez/Rei.cs
+++ xadreX/xadrez/Rei.cs
@@ -101,6 +101,19 @@ namespace xadrez
                     }
                 }
 
+                // Roque Grande
+                Posicao postT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(postT2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+
             }
 
             return mat;
